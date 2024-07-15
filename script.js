7979a4e933c2b5679be1b7360b23bc83a27b25b5
theguffs <youssefabdelmalak01@gmail.com>
2024-07-15 17:30:59 +0200

    esercizio finito + bonus

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-for (let i=1 ; i<=100 ; i++){
-    if((i % 3 == 0) && (i % 5 == 0)){
-        console.log("Fizz-Buzz");
-    }
-    else if (i % 3 == 0) {
-        console.log("Fizz"); 
-    } 
-    else if (i % 5 == 0) {
-        console.log("Buzz");
-    }
-    else{
-        console.log(i)
-    }
+    let container = document.getElementById('fizzBuzzContainer');
+
+    for (let i = 1; i <= 100; i++) {
+        let elemento = document.createElement('div');
+        elemento.classList.add( 'quadrato','d-flex','justify-content-center','m-1');
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            elemento.textContent = 'FizzBuzz';
+            elemento.classList.add('bg-warning');
+        } 
+        else if (i % 3 == 0) {
+            elemento.textContent = 'Fizz';
+            elemento.classList.add('bg-primary');
+        } 
+        else if (i % 5 == 0) {
+            elemento.textContent = 'Buzz';
+            elemento.classList.add('bg-danger');
+        } 
+        else {
+            elemento.textContent = i;
+            elemento.classList.add('bg-success');
+        }
+
+        container.append(elemento);
 }
